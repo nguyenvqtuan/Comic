eca9932befa57ae51e76962659173566262edc67
quoc tuan <nvtuanq123@gmail.com>
2024-03-19 09:29:13 +0700

    Config add eureka client

--- manage/src/main/java/com/comic/manage/ManageApplication.java
+++ manage/src/main/java/com/comic/manage/ManageApplication.java
@@ -2,8 +2,9 @@ package com.comic.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ManageApplication {
 
 	public static void main(String[] args) {
