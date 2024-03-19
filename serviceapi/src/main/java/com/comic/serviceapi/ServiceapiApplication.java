8f0a4349befdfd00b7d387c810b7e77dc43914b2
quoc tuan <nvtuanq123@gmail.com>
2024-03-19 10:48:34 +0700

    Feat Add use Slf4j

--- serviceapi/src/main/java/com/comic/serviceapi/ServiceapiApplication.java
+++ serviceapi/src/main/java/com/comic/serviceapi/ServiceapiApplication.java
@@ -3,11 +3,15 @@ package com.comic.serviceapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class ServiceapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceapiApplication.class, args);
+		log.info("Start server");
 	}
 
 }
