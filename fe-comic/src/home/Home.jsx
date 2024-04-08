069718c78d0ee9957a57034b48e23c6e2ef49b5f
quoc tuan <nvtuanq123@gmail.com>
2024-04-08 11:11:18 +0700

    Fix Change directory index to home

---
+++ fe-comic/src/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Rating from './Rating'
+import Lastest from './Lastest'
+import Carousel from './Carousel'
+
+const Home = () => {
+  return (
+    <div>
+        <Rating />
+        <Lastest />
+        <Carousel />
+    </div>
+  )
+}
+
+export default Home
