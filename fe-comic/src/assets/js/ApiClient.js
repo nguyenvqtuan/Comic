144019d4a4bf566ff37111b075a53d144db7cb8e
quoc tuan <nvtuanq123@gmail.com>
2024-04-13 09:56:10 +0700

    Feat Add file config.json

--- fe-comic/src/assets/js/ApiClient.js
+++ fe-comic/src/assets/js/ApiClient.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
+import config from '../../../config.json'
 
 export default axios.create({
-    baseURL: "https://webtoon.p.rapidapi.com",
-    headers: {
-        'X-RapidAPI-Key': '62ade18fc8msh0ab50b44dc07433p1da38ejsne433e1369aa8',
-        'X-RapidAPI-Host': 'webtoon.p.rapidapi.com'
-    }
+    baseURL: config.SERVER
 })
