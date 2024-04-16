72f6232dcc4b09c43ef88d5bd8ea38f0be78acc8
quoc tuan <nvtuanq123@gmail.com>
2024-04-16 08:02:22 +0700

    Feat Add config enable CORS policy for front end

---
+++ serviceapi/src/main/java/com/comic/serviceapi/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.comic.serviceapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	  public void addCorsMappings(CorsRegistry registry) {
+
+	    registry.addMapping("/**")
+	      .allowedOrigins("http://localhost:5173")
+	      .allowedMethods("GET", "POST")
+	      .allowedHeaders("header1", "header2", "header3")
+	      .exposedHeaders("header1", "header2")
+	      .allowCredentials(true).maxAge(3600);
+	  }
+}
